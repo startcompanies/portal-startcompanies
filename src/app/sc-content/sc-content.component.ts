b3051b831a27e42f3c0d677b52a655b8d0b3de0e
AbisurDiazR <abisurdiazramirez@gmail.com>
2025-07-23 01:04:07 -0600

    Change logo color

--- src/app/sc-content/sc-content.component.ts
+++ src/app/sc-content/sc-content.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ScHeaderComponent } from '../sc-header/sc-header.component';
 import { ScFooterComponent } from '../sc-footer/sc-footer.component';
 
 @Component({
   selector: 'app-sc-content',
   standalone: true,
-  imports: [
-    ScHeaderComponent,
-    ScFooterComponent
-  ],
+  imports: [ScHeaderComponent, ScFooterComponent],
   templateUrl: './sc-content.component.html',
-  styleUrl: './sc-content.component.css'
+  styleUrl: './sc-content.component.css',
 })
-export class ScContentComponent {
+export class ScContentComponent implements OnInit {
+  isOpen: boolean = false;
 
+  ngOnInit(): void {}
+
+  toggleMenu(): void {
+    this.isOpen = !this.isOpen;
+  }
 }
--- src/app/sc-header/sc-header.component.ts
+++ src/app/sc-header/sc-header.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SharedModule } from '../shared/shared/shared.module';
 
 @Component({
   selector: 'app-sc-header',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './sc-header.component.html',
-  styleUrl: './sc-header.component.css'
+  styleUrl: './sc-header.component.css',
 })
-export class ScHeaderComponent {
+export class ScHeaderComponent implements OnInit {
+  isOpen: boolean = false;
+  isNavbarShrunk: boolean = false;
 
+  ngOnInit(): void {}
+
+  toggleMenu(): void {
+    this.isOpen = !this.isOpen;
+  }
 }
