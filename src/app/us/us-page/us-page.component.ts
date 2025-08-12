633c82e159f6317305311fa9d3df669eef6b96c6
AbisurDiazR <abisurdiazramirez@gmail.com>
2025-08-11 22:30:15 -0600

    Bugfix icon sizes on benefits

--- src/app/us/us-page/us-page.component.ts
+++ src/app/us/us-page/us-page.component.ts
@@ -5,11 +5,12 @@ import { TranslocoPipe } from '@jsverse/transloco';
 import { UsStrategiesComponent } from '../us-strategies/us-strategies.component';
 import { UsPotentialComponent } from '../us-potential/us-potential.component';
 import { UsProposalComponent } from "../us-proposal/us-proposal.component";
+import { VideoSectionComponent } from "../../landings/video-section/video-section.component";
 
 @Component({
   selector: 'app-us-page',
   standalone: true,
-  imports: [ScHeaderComponent, ScFooterComponent, TranslocoPipe, UsStrategiesComponent, UsPotentialComponent, UsProposalComponent, UsProposalComponent],
+  imports: [ScHeaderComponent, ScFooterComponent, TranslocoPipe, UsPotentialComponent, UsProposalComponent, UsProposalComponent, VideoSectionComponent],
   templateUrl: './us-page.component.html',
   styleUrl: './us-page.component.css'
 })
