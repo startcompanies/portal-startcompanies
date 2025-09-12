ec2162193baf908780f016662ab75636953609f5
AbisurDiazR <abisurdiazramirez@gmail.com>
2025-09-12 10:30:29 -0600

    API Blog

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000',
+  //apiUrl: 'https://bot-start-companies-backend-90fmsk-98fff6-168-231-74-52.traefik.me',
+  apiUrl: 'https://api-web.startcompanies.us'
 };
