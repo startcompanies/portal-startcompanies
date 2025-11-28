9d4988daaf82c8b131e9823f5bc2cab3b9d5ec8f
benjarmc <benjamin@digitalconquest.mx>
2025-11-27 21:48:56 -0600

    fix: change api domain staging

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://api-web.startcompanies.io',
+  apiUrl: 'https://api-web.startcompanies.us',
   baseUrl: 'https://startcompanies.io',
   domain: 'startcompanies.io'
 };
