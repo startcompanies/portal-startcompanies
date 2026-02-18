e1be30c45e68be62d97dec9c48d22540ff386c63
benjarmc <benjamin@digitalconquest.mx>
2026-02-17 21:46:59 -0600

    Update environment configuration and refine button styles in wizard steps
    - Changed API and base URLs in the production environment configuration from 'startcompanies.us' to 'startcompanies.io' for consistency.
    - Refined CSS for button styles in both the 'wizard-cuenta-bancaria' and 'wizard-renovacion-llc' steps to improve visual consistency when buttons are disabled.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   staging: false,
-  apiUrl: 'https://api-web.startcompanies.us',
+  apiUrl: 'https://api-web.startcompanies.io',
   postsEndpoint: '/blog/posts/get-from-portal',
   categoriesEndpoint: '/blog/categories/with-posts-count',
   sandboxPostsEndpoint: '/blog/posts/get-sandbox-posts/category',
@@ -10,9 +10,9 @@ export const environment = {
     relayPixelId: '1055049486479771',
     debug: false // Modo debug desactivado en producción
   },
-  baseUrl: 'https://startcompanies.us',
+  baseUrl: 'https://startcompanies.io',
   domain: 'startcompanies.us',
   stripe: {
-    publishableKey: 'pk_live_51...' // Clave de producción
+    publishableKey: '' // Clave de producción
   }
 };
